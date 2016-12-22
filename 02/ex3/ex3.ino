a6dfb0fa6984d44f5b7e2cb341dd54c972e1734e
Tetsuya Kanja <kan.tetsuya@gmail.com>
2016-12-22 23:00:27 +0900

    チャタリング防止機能追加

--- 02/ex3/ex3.ino
+++ 02/ex3/ex3.ino
@@ -8,13 +8,30 @@ static const uint8_t LEDPin = 13;
 static uint8_t lastSW = LOW;
 static uint8_t currentLED = LOW;
 
+static uint8_t ChatteringFilter(uint8_t val) {
+  static const unsigned long chatteringMs = 50; // チャタリング防止する時間
+  static unsigned long lastChangeTime = 0;  // 最後に変化した時刻
+  static uint8_t filteredVal = LOW;
+  static uint8_t lastVal = LOW;
+  unsigned long ms = millis();
+  if(lastVal != val) {
+    // 前の状態から変化したら現在時刻でリセット
+    lastChangeTime = ms;
+  }
+  if(chatteringMs <= ms - lastChangeTime) {
+    // 最後に変化してから一定時間経過していたら切替え
+    filteredVal = val;
+  }
+  lastVal = val;
+}
+
 void setup() {
   pinMode(SWPin, INPUT_PULLUP);
   pinMode(LEDPin, OUTPUT);
 }
 
 void loop() {
-  uint8_t currentSW = digitalRead(SWPin);
+  uint8_t currentSW = ChatteringFilter(digitalRead(SWPin));
   if(lastSW == HIGH && currentSW == LOW) {
     // OFF→ONになったときに切り替える
     if(currentLED == LOW) {
